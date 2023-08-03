e7fcd2c4594bd6a275f377e710c1e88fde60d72a
Lowe Löwing <lowe.lowing@gmail.com>
2023-08-03 09:26:25 +0200

    middleware auth Cookie added

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,24 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
 export default async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
-  const session = !!req.cookies.get("next-auth.session-token")
+  const session = !!(
+    req.cookies.get("next-auth.session-token") ||
+    req.cookies.get("__Secure-next-auth.session-token")
+  );
 
   if (!session) {
-    return NextResponse.redirect(new URL(`/api/auth/signin?callbackUrl=${path}`, req.url));
+    return NextResponse.redirect(
+      new URL(`/api/auth/signin?callbackUrl=${path}`, req.url)
+    );
   }
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)','/boards', '/workspace/:path*']
-}
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+    "/boards",
+    "/workspace/:path*",
+  ],
+};
