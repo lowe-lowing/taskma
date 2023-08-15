1f94d9c79968b5e57c631d0d7aa3108993820a9a
Lowe Löwing <lowe.lowing@gmail.com>
2023-08-15 20:10:16 +0200

    auth env providers removed

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -8,7 +8,6 @@ import { env } from "@/env/server.mjs";
 import { prisma } from "@/server/db/client";
 
 export const authOptions: NextAuthOptions = {
-  // Include user.id on session
   callbacks: {
     session({ session, user }) {
       if (session.user) {
@@ -17,22 +16,12 @@ export const authOptions: NextAuthOptions = {
       return session;
     },
   },
-  // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
-    GitHubProvider({
-      clientId: env.GITHUB_ID,
-      clientSecret: env.GITHUB_SECRET,
-    }),
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
-    // ...add more providers here
   ],
 };
 
