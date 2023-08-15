8a02d85fdb0df00538c2e46e47b13ab9ed93c571
Lowe Löwing <lowe.lowing@gmail.com>
2023-08-15 21:15:04 +0200

    build error

--- src/server/common/get-server-auth-session.ts
+++ src/server/common/get-server-auth-session.ts
@@ -1,7 +1,6 @@
+import { authOptions } from "@/lib/auth";
 import { type GetServerSidePropsContext } from "next";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../../pages/api/auth/[...nextauth]";
-
 /**
  * Wrapper for unstable_getServerSession https://next-auth.js.org/configuration/nextjs
  * See example usage in trpc createContext or the restricted API route
